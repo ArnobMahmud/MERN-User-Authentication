226c4040a62b331b712bb14613e659f193cd38dd
Arnob Mahmud <arnob.tech.me@gmail.com>
2022-01-19 23:03:10 +0600

    client site added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Users from './pages/Users';
+import Activated from './pages/Activated';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
+import Layout from './hoc/Layout';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/activated" component={Activated} />
+        </Switch>
+      </Layout>
+    </Router>
+  </Provider>
+);
 
 export default App;
